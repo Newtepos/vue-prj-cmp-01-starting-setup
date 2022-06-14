263438c421c789236f1aaa4dec6976142a6482a6
Supakit <supakit.pnm@gmail.com>
2022-06-14 15:44:33 +0700

    Add: BaseButton

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const VueApp = createApp(App);
 VueApp.component('base-card', BaseCard);
+VueApp.component('base-button', BaseButton);
 VueApp.mount('#app');
