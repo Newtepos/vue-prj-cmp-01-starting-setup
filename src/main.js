04fd27238238ce27560e0c9d531fd9f76bd84708
Supakit <supakit.pnm@gmail.com>
2022-06-14 17:02:57 +0700

    Add: Alert Digalog

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
 const VueApp = createApp(App);
 VueApp.component('base-card', BaseCard);
 VueApp.component('base-button', BaseButton);
+VueApp.component('base-dialog', BaseDialog);
 VueApp.mount('#app');
