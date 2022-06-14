b9c17b6cba3f045283627a6c327cc1de4c88b557
Supakit <supakit.pnm@gmail.com>
2022-06-14 15:38:19 +0700

    Add: BaseCard Compoennt

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-
+import BaseCard from './components/UI/BaseCard.vue';
 
 const VueApp = createApp(App);
+VueApp.component('base-card', BaseCard);
 VueApp.mount('#app');
